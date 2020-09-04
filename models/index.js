a6f338b0046c209875e0312fc5a1cfeab90ad535
lafry5 <lafry5@yahoo.com>
2020-09-03 20:21:09 -0700

    added files

---
+++ models/index.js
@@ -0,0 +1,8 @@
+//import all models
+// example const employee = require('./employee');
+
+//create associations
+// example employee.hasMany...or belongsTo...
+
+// ends with....
+// module.exports = { User, Post, Vote, Comment };
