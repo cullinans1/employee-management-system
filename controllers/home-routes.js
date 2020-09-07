2ce9fd186cc6c5e11f9e8b12911561e74e43e6a0
Sammy <sammy.cullinan@gmail.com>
2020-09-07 13:29:28 -0700

    fixed cannot get login error

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,14 +1,22 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment, Vote } = require("../models");
 
 // get info for homepage
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/dashboard");
-     return;
-     }
-       res.render("homepage");
-     });
+    return;
+  }
+  res.render("homepage");
+});
 
-  module.exports = router;
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+module.exports = router;
