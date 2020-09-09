ac1482e5ef4511c6c3d70161b423f909dc64bc5a
lafry5 <lafry5@yahoo.com>
2020-09-09 09:12:04 -0700

    dashboard, single employees view, view all employees and view all hours

--- controllers/api/employee-routes.js
+++ controllers/api/employee-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 // const sequelize = require('../../config/connection');
 const { Admin, Employee } = require("../../models");
 
-router.get("/", (req, res) => {
+router.get("/view", (req, res) => {
   Employee.findAll({
     attributes: ["id", "username", "email", "role", "pto", "holiday", "sick"],
   })
